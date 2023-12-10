6ec195662e9ecc24b51b54889135362095d72ba9
reemnassif20 <reem.ibrahim@el-eng.menofia.edu.eg>
2023-12-10 22:06:54 +0200

    Added Home GUI & added navigation Buttons in All of GUIs

--- src/main/java/Controllers/SceneController.java
+++ src/main/java/Controllers/SceneController.java
@@ -5,7 +5,6 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class SceneController {
@@ -20,7 +19,6 @@ public class SceneController {
     stage.setScene(scene);
     stage.show();
 
-
     }
     public void SwitchToSignUp(ActionEvent event) throws IOException {
         root=FXMLLoader.load(getClass().getResource("/GUI files/SignUP.fxml"));
@@ -29,4 +27,48 @@ public class SceneController {
         stage.setScene(scene);
         stage.show();
     }
+    public void SwitchToHome(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/Home.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToHistory(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/History.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToSettings(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/Settings.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToExercising(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/Exercising.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToMeals(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/Meals.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void SwitchToAlarms(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(getClass().getResource("/GUI files/Alarms.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,6 @@
 module com.example.healthtrackingsystem {
     requires javafx.controls;
     requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
     opens com.example.healthtrackingsystem to javafx.fxml;
     opens Controllers to javafx.fxml;
     exports com.example.healthtrackingsystem;
