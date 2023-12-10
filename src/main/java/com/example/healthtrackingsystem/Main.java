759c9869ee76cc2cbde4160e06b9a08da7926965
reemnassif20 <reem.ibrahim@el-eng.menofia.edu.eg>
2023-12-10 15:00:45 +0200

    Added SignUp &SignIn GUIs

--- src/main/java/com/example/healthtrackingsystem/Main.java
+++ src/main/java/com/example/healthtrackingsystem/Main.java
@@ -4,18 +4,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/GUI files/main.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/GUI files/SignUP.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
-        String cssFile = getClass().getResource("/CSS files/styles.css").toExternalForm();
-        scene.getStylesheets().add(cssFile);
         stage.setTitle("Health Tracking System");
         Image icon =new Image(getClass().getResourceAsStream("/Images/logo.jpeg"));
         stage.getIcons().add(icon);
